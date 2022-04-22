383f83231b74ce0cbfba533ef71115c2cd62bf96
Emerson Dantas <emerson.dantaspereira@hotmail.com>
2022-04-22 17:09:39 -0300

    Criado Model de Usuario

---
+++ src/model/User.js
@@ -0,0 +1,59 @@
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const db = require('../database/index');
+
+const User = db.define("users", {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    birthdate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+    },
+    document: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    acceptedTerms: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+    },
+    accessCount: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    zipcode: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    street: {
+        type: Sequelize.STRING,
+    },
+    neighborhood: {
+        type: Sequelize.STRING,
+    },
+    city: {
+        type: Sequelize.STRING,
+    },
+    state: {
+        type: Sequelize.STRING,
+    },
+    createdAt:{
+        type: Sequelize.DATE,
+    },
+    updatedAt: {
+        type: Sequelize.DATE,
+    }
+});
+
+/*(async () => {
+  await User.sync({force: true});
+})();*/
+
+module.exports = User;
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,17 @@
 const express = require('express');
 
+const User = require('../model/User')
+
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ Funcionado: 'Hello world' })
-});
+routes.post('/create', async (req, res) => {
+    try{
+        const user = await User.create({...req.body, updatedAt: null});
+
+        return res.send({ user });
+    } catch (err){
+        return res.status(400).send({ error: 'Registration failed' });
+    }
+})
 
 module.exports = routes;
